aa341fd17758e75c815bc188a7ab5f49bded65e5
Heman <heman.1682@gmail.com>
2023-12-27 21:38:34 +0330

    session 02 - fork child process

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
-print("Hello World!!!")
+import os
+
+
+def run():
+    pid = os.fork()
+
+    if pid==0:
+        os.execvp('/bin/bash',['/bin/bash'])
+    else:
+        rid, status = os.waitpid(pid, 0)
+        print(rid, status)
+
+
+if __name__ == '__main__':
+    run()
