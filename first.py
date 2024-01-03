7cb02cd3bd4f1e009334c2a04a707d022cb319eb
Heman <heman.1682@gmail.com>
2024-01-03 21:01:53 +0330

    session 03 - chroot

--- first.py
+++ first.py
@@ -5,9 +5,13 @@ def run():
     pid = os.fork()
 
     if pid==0:
-        os.execvp('/bin/bash',['/bin/bash'])
+        os.system("mount -t proc none /vagrant/myroot/proc ")
+        os.chroot("/vagrant/myroot")
+        os.chdir("/")
+        os.execvp('/bin/busybox',['/bin/busybox', 'sh'])
     else:
         rid, status = os.waitpid(pid, 0)
+        os.system("umount /vagrant/myroot/proc ")
         print(rid, status)
 
 
