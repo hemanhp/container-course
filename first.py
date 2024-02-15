42a57fcb248e7bc6a5a152d63e60dd3fe8e68dac
Heman <heman.1682@gmail.com>
2024-02-15 17:48:01 +0330

    session 05 - pid and uts

--- first.py
+++ first.py
@@ -4,9 +4,8 @@ import ctypes
 libc = ctypes.CDLL('libc.so.6', use_errno=True)
 
 CLONE_NEWNS = 	0x00020000
-
-
-
+CLONE_NEWUTS = 	0x04000000
+CLONE_NEWPID =	0x20000000
 def unsharens(flag):
     result = libc.unshare(flag)
 
@@ -15,13 +14,21 @@ def unsharens(flag):
         print(err_no)
 
 
+def set_hostname(name):
+    result = libc.sethostname(name.encode(), len(name))
+    if result < 0:
+        err_no = ctypes.get_errno()
+        print(err_no)
 
 
 def run():
+    unsharens(CLONE_NEWPID)
     pid = os.fork()
 
     if pid==0:
-        unsharens(CLONE_NEWNS)
+        unsharens(CLONE_NEWNS | CLONE_NEWUTS)
+
+        set_hostname("front")
         os.system("mount -t proc none /vagrant/myroot/proc ")
         os.chroot("/vagrant/myroot")
         os.chdir("/")
